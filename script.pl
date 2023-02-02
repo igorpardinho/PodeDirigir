4f2990110f2ac8071a8bc7a5698969b92f2abbd2
igorpardinho <pardinhoigor@gmail.com>
2023-02-02 13:33:36 -0300

    update files

--- script.pl
+++ script.pl
@@ -1,20 +1,38 @@
+#!/usr/bin/perl
+
+
 use warnings;
 
+system("sudo apt-get install figlet");
+
+print "Digite seu nome: ";
+
+my $nome = <STDIN>;
+
 print "Digite sua idade: ";
+
 my $idade = <STDIN>;
 
-if($idade >= 18){
 
-    print "Você tem carteira de motorista?: ";
-    my $carteira = <STDIN>;
-    chomp($carteira);
+sub pilotos {
 
-    if(($carteira eq "sim") || ($carteira eq "Sim")){
-        print "Parabéns piloto :)";
-    } else{
-        print "Que pena, ainda não pode dirigir.";
-    }
 
-} else{
- print "Não pode dirigir.";
+ $piloto =  $_[0];
+   system("figlet Parabens $piloto");
 }
+
+
+if($idade >= 18){
+    print "Você tem habilitação?: ";
+    my $habilitacao = <STDIN>;
+    chomp($habilitacao);
+
+        if($habilitacao eq "sim" || $habilitacao eq "Sim"){
+                print &pilotos($nome);
+        } else{
+            print "Que penaaa...";
+        }
+
+ } else{
+     print "Não Pode dirigir";
+ }
